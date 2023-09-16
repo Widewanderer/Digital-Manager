5661bd75e38c9ffe8a16ed9e54f6ba9cbc934862
Tyson Trotz <slavatrotz@gmail.com>
2023-09-16 15:50:13 -0500

    add init function, added first button (quit)

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+const inquirer = require("inquirer");
 
+const questions = [
+  {
+    type: "list",
+    name: "TopMenu",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Department",
+      "Add Department",
+      "Quit",
+    ],
+  },
+];
 
+function handleQuit() {
+  console.log("Goodbye!");
+  process.exit(0); // Exit the program with a success status code (0)
+}
+
+// initiation of program
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const choice = answers.TopMenu;
+    //console.log(answers)
+    if (choice === "Quit") {
+      handleQuit();
+    } else {
+      console.log(`You selected ${choice}`);
+    }
+  });
+}
+
+init();
